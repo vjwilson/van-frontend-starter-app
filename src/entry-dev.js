95278d65d435fddec46e1fe2c6d03d229f23bbb7
Van Wilson <van.wilson1992@alumni.unc.edu>
2022-01-25 10:49:28 -0500

    Initialize features/cloudify

---
+++ src/entry-dev.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { init } from './store';
+import App from './App';
+import logger from 'redux-logger';
+
+/**
+ * Hooks up redux to app.
+ *  https://redux.js.org/advanced/usage-with-react-router
+ */
+ReactDOM.render(
+    <Provider store={ init(logger).getStore() }>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
+
+    document.getElementById('root')
+);
