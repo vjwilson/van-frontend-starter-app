c94284361a637470fe9300349f767d942ad1838a
Van Wilson <van.wilson1992@alumni.unc.edu>
2022-01-25 10:49:06 -0500

    Initialize minor-fixes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Routes } from './Routes';
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <Routes childProps={this.props} />
+        );
+    }
+}
+
+/**
+ * withRouter: https://reacttraining.com/react-router/web/api/withRouter
+ * connect: https://github.com/reactjs/react-redux/blob/master/docs/api.md
+ *          https://reactjs.org/docs/higher-order-components.html
+ */
+export default withRouter(connect()(App));
