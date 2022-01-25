d442a2d8e318aa3fda211b9beb682ced08638483
Van Wilson <van.wilson1992@alumni.unc.edu>
2022-01-25 10:49:32 -0500

    Initialize dependabot/npm_and_yarn/patternfly/react-table-2.28.39

---
+++ config/dev.webpack.config.js
@@ -0,0 +1,13 @@
+
+const { resolve } = require('path');
+const config = require('@redhat-cloud-services/frontend-components-config');
+const { config: webpackConfig, plugins } = config({
+    rootFolder: resolve(__dirname, '../'),
+    debug: true,
+    https: true
+});
+
+module.exports = {
+    ...webpackConfig,
+    plugins
+};
